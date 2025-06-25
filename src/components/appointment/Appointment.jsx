97f13d9ff8f0b9b5afc02e445311bf2d92448ffa
Aurore Reynier <reynier.aurore@gmail.com>
2025-06-25 16:05:46 +0200

    feat(appointment): do unit tests and refactoring on appointment component

--- src/components/appointment/Appointment.jsx
+++ src/components/appointment/Appointment.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Calendar from "./calendar/Calendar"
 import Form from "./form/Form"
+import ErrorModal from "./error-modal/ErrorModal"
 import './Appointment.scss'
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
     const [frontDate, setFrontDate] = useState("");
     const [backDate, setBackDate] = useState("2001-01-01T00:00:00.000Z");
     const[data, setData] =useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
 
     const handleToggleCalendarVisibility = () => {
         setShowCalendar(!showCalendar)
@@ -26,23 +30,33 @@ function App() {
         setBackDate(newBackDate);
       };
 
-      useEffect(() => {
-        const fetchAppointment = async () => {
-          try {
-            const response = await fetch('https://truckbusters-backend.onrender.com/api/v1/appointment' || 'http://localhost:3002/api/v1/appointment');
-            if (!response.ok) {
-              throw new Error('Failed to fetch data');
-            }
-            const result = await response.json();
-            setData(result);
+      const fetchAppointment = async () => {
+        setIsLoading(true);
+        setError(null);
 
-          } catch (error) {
-            console.error('Error fetching data:', error);
+        try {
+          const response = await fetch('https://truckbusters-backend.onrender.com/api/v1/appointment' || 'http://localhost:3002/api/v1/appointment');
+          if (!response.ok) {
+            throw new Error('Failed to fetch data');
           }
-        };
+          const result = await response.json();
+          setData(result);
+
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          setError(error);
+        } finally {
+          setIsLoading(false);
+        }
+      };
       
+      useEffect(() => {
         fetchAppointment();
       }, []);
+
+      const handleCloseError = () => {
+        setError(null);
+    };
 
       let dataArray = [];
 
@@ -74,8 +88,27 @@ function App() {
           <h2 className='appointment__title'>Prendre rendez-vous</h2>
           <h2 className='appointment__title'>/ / / / / /</h2>
         </div>
-        <Calendar duplicateMoments={duplicateMoments} configureFrontDate={updateFrontDateState} configureBackDate={updateBackDateState} calendarToggleVisibility={handleToggleCalendarVisibility} formToggleVisibility={handleToggleFormVisibility} displayCalendar={showCalendar} displayForm={showForm} />
-        <Form getFrontDate={frontDate} getBackDate={backDate} calendarToggleVisibility={handleToggleCalendarVisibility} formToggleVisibility={handleToggleFormVisibility} displayForm={showForm} />
+
+        {isLoading && (
+          <div className="loading">
+            <p>Chargement en cours...</p>
+          </div>
+        )}
+        
+        {error && (
+          <ErrorModal 
+            open={!!error}
+            onClose={handleCloseError}
+          />
+        )}
+
+        {!isLoading && !error && (
+          <>
+            <Calendar duplicateMoments={duplicateMoments} configureFrontDate={updateFrontDateState} configureBackDate={updateBackDateState} calendarToggleVisibility={handleToggleCalendarVisibility} formToggleVisibility={handleToggleFormVisibility} displayCalendar={showCalendar} displayForm={showForm} />
+            <Form getFrontDate={frontDate} getBackDate={backDate} calendarToggleVisibility={handleToggleCalendarVisibility} formToggleVisibility={handleToggleFormVisibility} displayForm={showForm} />
+          </>
+        )}
+
       </>
     )
   }
